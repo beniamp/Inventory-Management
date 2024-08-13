02809cc77bccc98da14fffef6feaa563036b14a3
beniamp <102410523+beniamp@users.noreply.github.com>
2024-08-13 11:54:07 +0330

    Update balance_dash.py

--- balance_dash.py
+++ balance_dash.py
@@ -177,11 +177,11 @@ def determine_action_status(product_data):
     
     if restock_point > 1:
         return "Brown Type 1"
-    elif 0.1 < restock_point <= 1 and stock != 0:
+    elif 0.05 < restock_point <= 1 and stock != 0:
         return "Red"
-    elif 0.01 < restock_point <= 0.1 and round(product_data['MaxAvailability'] / product_data['Order_Rate']) < 30:
+    elif 0.01 < restock_point <= 0.05 and round(product_data['MaxAvailability'] / product_data['Order_Rate']) < 30:
         return "Yellow"
-    elif 0.01 < restock_point <= 0.1 and round(product_data['MaxAvailability'] / product_data['Order_Rate']) > 30:
+    elif 0.01 < restock_point <= 0.05 and round(product_data['MaxAvailability'] / product_data['Order_Rate']) > 30:
         return 'Green'
     elif 0.001 < restock_point < 0.01 or round(product_data['MaxAvailability'] / product_data['Order_Rate']) > 90:
         return "Brown Type 2"
