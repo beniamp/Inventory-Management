32bae99c72f2a508c21a449cf674edc2c5fcdc49
beniamp <102410523+beniamp@users.noreply.github.com>
2024-08-13 11:25:22 +0330

    Update balance_dash.py

--- balance_dash.py
+++ balance_dash.py
@@ -236,7 +236,7 @@ st.markdown("""
     .box-brown2 { background-color: #cc7700; }
     </style>
     <div class="custom-box box-brown">
-        💩
+        💩<br>
         تست
     </div>
 """, unsafe_allow_html=True)
