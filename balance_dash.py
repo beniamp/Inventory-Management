4a93d82419ce5d8c9450286a07dfd5a27b129f67
beniamp <102410523+beniamp@users.noreply.github.com>
2024-08-14 13:05:35 +0330

    Update balance_dash.py

--- balance_dash.py
+++ balance_dash.py
@@ -169,9 +169,9 @@ final_df = merged_df.groupby('ProductColorName', as_index=False).agg({
 df8 = final_df[(merged_df['Date'].isna()) & (merged_df['Quantity_stock'] != 0)]
 
 # Step 2: Replace values based on the given conditions
-df8.loc[:, 'ProductNameColor'] = df8['ProductNameColor'].fillna(df8['ProductColorName'])
-df8.loc[:, 'Quantity_order'] = df8['Quantity_stock'].fillna(0)
-df8.loc[:, 'Category_order'] = df8['CategoryS'].fillna(df8['Category_stock'])
+#df8.loc[:, 'ProductNameColor'] = df8['ProductNameColor'].fillna(df8['ProductColorName'])
+#df8.loc[:, 'Quantity_order'] = df8['Quantity_stock'].fillna(0)
+#df8.loc[:, 'Category_order'] = df8['CategoryS'].fillna(df8['Category_stock'])
 
 # df8 now contains the filtered and updated data
 
