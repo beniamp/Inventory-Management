84cd3cd1992282a5fa2b5a72d898f7de52432420
beniamp <102410523+beniamp@users.noreply.github.com>
2024-08-17 14:40:20 +0330

    Update balance_dash.py

--- balance_dash.py
+++ balance_dash.py
@@ -38,10 +38,7 @@ import numpy as np
 
 #result = cursor.execute(query1).fetchall()
 # Page setting
-st.set_page_config(layout="wide")
 
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
 
