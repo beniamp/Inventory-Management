d8a3fa0a619cf5d1bc6812c00822a6f14d75476a
beniamp <102410523+beniamp@users.noreply.github.com>
2024-08-24 15:13:06 +0330

    Update balance_dash.py

--- balance_dash.py
+++ balance_dash.py
@@ -278,7 +278,7 @@ product_max_availability = filtered_df.groupby('Product')['Availability'].max().
 product_data_dt = pd.merge(product_total_volume2, product_max_availability, on='Product')
 
 # Calculate Order Rate (Orders Per Day)
-product_data_dt['Order_Rate'] = product_data_dt['Volume'] / count_days
+product_data_dt['Order_Rate'] = product_data_dt['Volume'] / count_dates
 
 # Calculate Stock Ratio
 product_data_dt['Restock_Ratio'] = product_data_dt['Order_Rate'] / product_data_dt['Availability'].replace(0, 0.1)
